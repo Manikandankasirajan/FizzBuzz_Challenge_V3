8ac9d8c047bc88a76fa23da08174f86acd52a089
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-01 11:50:29 +0530

    feat : add feature that returns 'Buzz' if the number is divisible by 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,8 @@
 export default function fizzbuzz(num) {
 	if (num % 3 === 0) {
 		return "Fizz";
+	} else if (num % 5 === 0) {
+		return "Buzz";
 	}
 	return num;
 }
--- src/tests/fizzbuzz.test.js
+++ src/tests/fizzbuzz.test.js
@@ -1,4 +1,5 @@
 import fizzbuzz from "../fizzbuzz";
+
 test("returns number", () => {
 	const num = 1;
 	expect(fizzbuzz(num)).toBe(1);
@@ -7,4 +8,9 @@ test("returns number", () => {
 test("returns Fizz if number is divisible by 3", () => {
 	const num = 3;
 	expect(fizzbuzz(num)).toBe("Fizz");
+});
+
+test("returns Buzz if number is divisible by 5", () => {
+	const num = 5;
+	expect(fizzbuzz(num)).toBe("Buzz");
 });

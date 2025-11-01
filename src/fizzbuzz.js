2b6a6a9e6531cab9743c90f1f882fec97e60d008
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-01 11:48:17 +0530

    feat : add feature that returns 'Fizz' if the number is divisible by 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,6 @@
 export default function fizzbuzz(num) {
+	if (num % 3 === 0) {
+		return "Fizz";
+	}
 	return num;
 }
--- src/tests/fizzbuzz.test.js
+++ src/tests/fizzbuzz.test.js
@@ -3,3 +3,8 @@ test("returns number", () => {
 	const num = 1;
 	expect(fizzbuzz(num)).toBe(1);
 });
+
+test("returns Fizz if number is divisible by 3", () => {
+	const num = 3;
+	expect(fizzbuzz(num)).toBe("Fizz");
+});
